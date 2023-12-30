0702ecbef312c606220af3b3fc1bfbcb9ee60621
muhammad <git@github.com:KingCanute1011>
2023-12-30 22:23:59 +0200

    commited

---
+++ src/test/java/RESTProject/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package RESTProject.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
